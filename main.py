d3b43733599e428114bbc85fc6e2111e37e823bb
Jack Fischer <jfische8@binghamton.edu>
2017-04-21 18:12:32 -0400

    housekeeping

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import tkinter #for graphics
-import csv #to read CSV spreadsheet
+import tkinter
+import csv
 
 #Set up window
-window = tkinter.Tk() #generate window
-canvas = tkinter.Canvas(window, width=500, height=500) #set up canvas to draw in it
-canvas.grid() #set cavnas to grid mode; special tkinter thing.
+window = tkinter.Tk()
+canvas = tkinter.Canvas(window, width=500, height=500)
+canvas.grid()
 
 #Turn spreadsheet into 2D list
-f = open("board template.csv") #open spreadsheet csv file
-spreadsheet = csv.reader(f, dialect='excel') #interpret file as CSV spreadsheet
-spreadsheet = list(spreadsheet) #transform into a 2D list
+f = open("board template.csv")
+spreadsheet = csv.reader(f, dialect='excel')
+spreadsheet = list(spreadsheet)
 for line in spreadsheet:
   print(line)
 
 #Traverse 2D list and draw board
-for row in range(10): #sheet rows
-  for col in range(10): #sheet columns
-    if spreadsheet[row][col] == 'X': #check if cell is X
+for row in range(10):
+  for col in range(10):
+    if spreadsheet[row][col] == 'X':
       y0 = row * 50
       x0 = col * 50
       y1 = y0 + 50
@@ -24,17 +24,17 @@ for row in range(10): #sheet rows
       canvas.create_rectangle(x0, y0, x1, y1, fill='pink')
 
 #Set up and draw player
-player_row = 0 #we want player oval to be small, so
-player_col = 0 #within a cell, from 10,10 to 40,40 instead of 0,0 to 50,50
-player_tkinter_id = None #will hold int identifying oval, so we can remove it #TODO try getting rid of
+player_row = 0
+player_col = 0
+player_tkinter_id = None #hold int identifying oval
 def redraw_player():
-  global player_tkinter_id #out of scope
-  canvas.delete(player_tkinter_id) #remove old oval
-  player_x = player_row * 50 + 10
-  player_y = player_col * 50 + 10
+  global player_tkinter_id
+  canvas.delete(player_tkinter_id)
+  player_x = player_col * 50 + 10
+  player_y = player_row * 50 + 10
   player_tkinter_id = canvas.create_oval(player_x, player_y, player_x+30, player_y+30, fill='blue') #create new oval
   print("Player now at %d,%d" % (player_x, player_y))
-redraw_player() #draw player for first time
+redraw_player() #draw intial player
 
 
 def no_wall(row: int, col: int) -> bool:
@@ -50,12 +50,12 @@ def no_wall(row: int, col: int) -> bool:
 
 
 #Functions called after keypresses
-def left(event): #called when a left keypress happens
-  global player_col #outside of scope
-  future_col = player_col - 1 #where it would go
-  if no_wall(player_row, future_col): #wall in the future spot?
-    player_col = future_col #if not, change coordinate
-  redraw_player() #delete the old player and put a new one with new coordinates
+def left(event):
+  global player_col
+  future_col = player_col - 1
+  if no_wall(player_row, future_col):
+    player_col = future_col
+  redraw_player()
 
 def right(event):
   global player_col
@@ -78,11 +78,9 @@ def down(event):
     player_row = future_row
   redraw_player()
 
-#Tell tkinter to call correct functions upon corresponding key presses
 window.bind('<Left>', left)
 window.bind('<Right>', right)
 window.bind('<Down>', down)
 window.bind('<Up>', up)
 
-#Tkinter should put the window up and start the event loop waiting for keys
 window.mainloop()
